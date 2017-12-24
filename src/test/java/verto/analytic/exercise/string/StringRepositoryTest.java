b6e57ac66a8ae841373dcea51985bbbbc3947c65
maxime.maigret <maxime.maigret@gmail.com>
2017-12-24 13:12:59 +0000

    delete : unit test red

--- src/test/java/verto/analytic/exercise/string/StringRepositoryTest.java
+++ src/test/java/verto/analytic/exercise/string/StringRepositoryTest.java
@@ -44,4 +44,30 @@ public class StringRepositoryTest {
         assertTrue("The stored strings must be the same as the entry one",stored.contains(initialEntry));
         assertTrue("The stored strings must be the same as the entry one",stored.contains(secondEntry));
     }
+
+    @Test
+    public void should_return_true_if_delete_string_exists(){
+        Set<String> initialEntry = new HashSet<>(Arrays.asList("aaa", "bbb", "ccc"));
+        repository.addSet(initialEntry);
+        Set<String> secondEntry = new HashSet<>(Arrays.asList("111", "222", "333"));
+        repository.addSet(secondEntry);
+
+        assertTrue("2 sets are stored in the stored set.", repository.getAllStoredSetString().size() == 2);
+
+        boolean result = repository.deleteSet(secondEntry);
+
+        assertTrue(result);
+        assertTrue("Only 1 set left in the stored set.", repository.getAllStoredSetString().size() == 1);
+        assertTrue("Initial entry left in the stored set.", repository.getAllStoredSetString().contains(initialEntry));
+    }
+
+    @Test
+    public void should_return_false_if_delete_string_not_exists(){
+        assertTrue("Empty stored set.", repository.getAllStoredSetString().size() == 0);
+
+        Set<String> notExistingEntry = new HashSet<>(Arrays.asList("aaa", "bbb", "ccc"));
+        boolean result = repository.deleteSet(notExistingEntry);
+
+        assertFalse(result);
+    }
 }
---
+++ src/test/java/verto/analytic/exercise/string/delete/DeleteServiceTest.java
@@ -0,0 +1,49 @@
+package verto.analytic.exercise.string.delete;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import verto.analytic.exercise.string.StringRepository;
+import verto.analytic.exercise.string.search.SearchService;
+
+import java.util.*;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.initMocks;
+
+public class DeleteServiceTest {
+
+    @InjectMocks
+    private DeleteService service;
+
+    @Mock
+    private StringRepository repository;
+
+    @Before
+    public void setUp() {
+        initMocks(this);
+    }
+
+    @Test
+    public void should_delete_set_if_exists(){
+        Set<String> setString = new HashSet<>(Arrays.asList("111", "222", "333"));
+        List<Set<String>> storedSet = Collections.singletonList(setString);
+
+        when(repository.deleteSet(setString)).thenReturn(Boolean.TRUE);
+
+        service.delete(setString);
+    }
+
+    @Test(expected = SetNotFoundException.class)
+    public void should_delete_throws_exception_if_not_exists(){
+
+        Set<String> setString = new HashSet<>(Arrays.asList("aaa", "bbb", "ccc"));
+
+        when(repository.deleteSet()).thenReturn(Boolean.FALSE);
+
+        service.delete(setString);
+    }
+
+}
